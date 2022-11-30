8824849259c4ade2b7251fb4b0c5497ef7a0cf6c
EC2 Default User <amrumineq@gmail.com>
2022-11-30 06:58:01 +0000

    Fix cart_items

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,29 +1,28 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cart_items = current_customer.cart_items
+    @cart_items = current_customer.cart_items.all
     @total_price = 0
   end
 
   def create
-    @cart_item = current_customer.cart_items.new(cart_item_params)
-    @cart_items=current_customer.cart_items.all
-    @cart_items.each do |cart_item|
-      if @cart_item.amount == nil
-        redirect_back(fallback_location: root_path)
-      elsif  cart_item.item_id == @cart_item.item_id
-        cart_item.amount += @cart_item.amount
-        @cart_item.delete
+    cart_item = current_customer.cart_items.new(cart_item_params)
+    current_cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    if params[:cart_item][:amount].blank?
+       redirect_back(fallback_location: root_path)
+    elsif current_cart_item.present?
+        current_cart_item.amount += params[:cart_item][:amount].to_i
+        current_cart_item.save
+        cart_item.delete
+        redirect_to cart_items_path
+    else
         cart_item.save
-      else
-        @cart_item.save
-      end
-      redirect_to  cart_items_path
+        redirect_to cart_items_path
     end
   end
 
   def update
-    @cart_item = CartItem.find(params[:id])
-    @cart_item.update(cart_item_params)
+    cart_item = CartItem.find(params[:id])
+    cart_item.update(cart_item_params)
     redirect_to cart_items_path
   end
 
